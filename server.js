338881a01473b7a60da88c69cb6fd50db75ef15c
cbowman422 <chrisb0wman@hotmail.com>
2023-01-28 06:29:56 -0500

    connected to mongodb with mongoose

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("be")
+// Mongoose is a Object Data Modeling (ODM) library for 
+// MongoDB distributed as an npm package.
+const mongoose = require("mongoose");
+
+// configures and gives access to .env file object
+require('dotenv').config();
+
+const { MONGODB_URI } = process.env
+
+
+mongoose.connect(MONGODB_URI)
+    .then( () => {
+        console.log(`[${new Date().toLocaleTimeString()}] - MongoDB connected ... 🙌 🙌 🙌`);
+    })
+    .catch( (error) => {
+        console.log('MongoDB connection error 😥', error)
+})
